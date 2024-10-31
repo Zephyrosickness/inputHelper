0544c9223f3e617f6ae44d0edeeb2bf5ee1405d8
Zephyros <zxphyros@gmail.com>
2024-10-31 09:46:34 -0400

    init

---
+++ src/InputHelper.java
@@ -0,0 +1,166 @@
+import java.util.*;
+
+public class InputHelper {
+    //do you think it would be better to initialize a public static final scanner up here so you dont have to initialize the scanner in every method or am i crazy
+
+    //[[[---INTEGERS---]]]
+
+    //checksum for valid int input | any values are accepted, as long as it's an int.
+    public static int getInt(final String message){
+        Scanner scan = new Scanner(System.in);
+        int input;
+        System.out.println(message);
+
+        do{
+            if(scan.hasNextInt()){
+                input = scan.nextInt();
+                break;
+
+            }else{System.out.println("Invalid input. Try again.");}
+
+        }while(true);
+
+        return input;
+    }
+
+    //checksum for valid, positive int input, (excl zero)
+    public static int getPositiveInt(final String message){
+        Scanner scan = new Scanner(System.in);
+        int input;
+        System.out.println(message);
+
+        do{
+            if(scan.hasNextInt()) {
+                input = scan.nextInt();
+                if (input>0) {break; //when input meets reqs
+
+                }else{System.out.println("Please enter a positive, non-zero integer value.");} //if input is an int, but is 0 or lower
+
+                }else{System.out.println("Invalid input. Try again.");} //if input is not int
+
+        }while(true);
+
+        return input;
+    }
+
+    //checksum for valid, positive int input within range
+    public static int getRangedInt(final String message, final int MIN, final int MAX){
+        Scanner scan = new Scanner(System.in);
+        int input;
+        System.out.println(message);
+        final String bounds = " [MIN: "+MIN+" MAX: "+MAX+"]"; //is appended to the end of any statements when the user enters invalid nums. example: "Invalid Input. Try again. [MIN: 0 MAX: 300]
+
+        do{
+            if(scan.hasNextInt()) {
+                input = scan.nextInt();
+                if (input>=MIN&&input<=MAX) {break; //when input meets reqs
+
+                }else{System.out.println("Please enter a positive, non-zero integer value."+bounds);} //if input is an int, but is 0 or lower
+
+            }else{System.out.println("Invalid input. Try again."+bounds);} //if input is not int
+
+        }while(true);
+
+        return input;
+    }
+
+
+
+    //[[[---STRINGS---]]]
+
+    //gets a string that is not null or a length of 0
+    public static String getString(final String message){
+        Scanner scan = new Scanner(System.in);
+        String input;
+        System.out.println(message);
+
+        //tbh idk the difference between isblank and isempty so i did both
+        do {
+            input = scan.nextLine();
+            if(!(input.isBlank() || input.isEmpty())){break;
+
+            }else{System.out.println("Sorry, you've entered an invalid response, try again. Please enter a valid, non-zero string.");}
+
+        }while (true);
+
+        return input;
+    }
+
+    //checksum for a nonzero-length string that is in a REGULAR NON-LIST ARRAY
+    //so if u have an array that's like [tomato, beans, carrots] you have to type in beans tomato or carrots
+    public static String getYN(final String message){
+        //init var
+        Scanner scan = new Scanner(System.in);
+        String input;
+        System.out.println(message+" [Y/N]");
+        final String[] options = {"y","n","yes","no"};
+
+        do{
+            input = scan.nextLine().toLowerCase(); //casts input to lower case to account for case differences
+
+            if(Arrays.asList(options).contains(input)) {break;
+
+                //casts options to arraylist because printing regular arrays isnt readable only lists are
+            }else{System.out.println("Sorry, you've entered an invalid input. Please try again. | VALID INPUTS: "+Arrays.asList(options));}
+
+        }while(true);
+
+        return input;
+    }
+
+    //takes inputs until it matches specified regex pattern
+    public static String getRegEx(final String message, final String regExPattern){
+        Scanner scan = new Scanner(System.in);
+        String input;
+
+        do {input = scan.nextLine();}while (!input.matches(regExPattern));
+
+        return input;
+    }
+
+    //[[[---EXTRAS---]]]
+    //stuff that wasnt on the methods list that i still thought would be a good idea to make because im bored
+
+    //checksum for a nonzero-length string that is in an arraylist
+    //so if u have an array that's like [tomato, beans, carrots] you have to type in beans tomato or carrots
+    public static String getStringInArrayList(final ArrayList<String> options, final String message){
+        //init var
+        Scanner scan = new Scanner(System.in);
+        String input;
+        System.out.println(message);
+
+        do{
+            input = scan.nextLine().toLowerCase(); //casts input to lower case to account for case differences
+
+            if(options.contains(input)) {break;
+
+            }else{System.out.println("Sorry, you've entered an invalid input. Please try again. | VALID INPUTS: "+options);}
+
+        }while(true);
+
+        return input;
+    }
+
+    //checksum for a nonzero-length string that is in a REGULAR NON-LIST ARRAY
+    //so if u have an array that's like [tomato, beans, carrots] you have to type in beans tomato or carrots
+    public static String getStringInArray(final String[] options, final String message){
+        //init var
+        Scanner scan = new Scanner(System.in);
+        String input;
+        System.out.println(message);
+        final ArrayList<String> optionsList = (ArrayList<String>)Arrays.asList(options);
+
+        do{
+            input = scan.nextLine().toLowerCase(); //casts input to lower case to account for case differences
+
+            if(optionsList.contains(input)) {break;
+
+            }else{System.out.println("Sorry, you've entered an invalid input. Please try again. | VALID INPUTS: "+optionsList);}
+
+        }while(true);
+
+        return input;
+    }
+
+    
+}
